4d879773538cb3e1f2751e9b2e4759bd4e416a4d
codingvaleria <codingvaleria@gmail.com>
2022-08-26 18:54:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email) {
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({ name, email })
+    }).then((response) => {
+        return response.json()
+
+    }).then((data) => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = data.id;
+        const content = document.getElementById('content');
+        content.appendChild(h2);
+
+    }).catch((error) => {
+        const p = document.createElement('p');
+        p.innerHTML = error.message;
+        const content = document.getElementById('content');
+        content.appendChild(p);
+    })
+}
+
